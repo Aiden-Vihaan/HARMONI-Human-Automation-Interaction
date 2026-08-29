5428db6afbbda09d63b3108bbe7451ece4687251
Aidycodes <aidenvihaan333@gmail.com>
2026-08-29 13:14:06 +0530

    Update index.ts

--- src/experiment/index.ts
+++ src/experiment/index.ts
@@ -3,3 +3,14 @@ export {
   type TransitionEvent,
   type TransitionRecord,
 } from './stateMachine'
+
+export {
+  EventLogger,
+  type EventContext,
+  type ExperimentEvent,
+} from './eventLogger'
+
+export {
+  TrialController,
+  type TrialControllerOptions,
+} from './trialController'
